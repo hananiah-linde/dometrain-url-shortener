bfcab9ad48f93f5cc3ff7b3fe146b03ff82335cc
hananiah-linde <hananiah.linde@gmail.com>
2024-12-18 14:43:08 -0600

    use token ranges

--- Api/src/UrlShortener.Core/Base62EncodingExtensions.cs
+++ Api/src/UrlShortener.Core/Base62EncodingExtensions.cs
@@ -3,14 +3,14 @@ namespace UrlShortener.Core;
 public static class Base62EncodingExtensions
 {
     public const string Alphanumeric = "0123456789" + "ABCDEFGHIJKLMNOPQRSTUVWXYZ" + "abcdefghijklmnopqrstuvwxyz";
-    public static string EncodeToBase62(this int number)
+    public static string EncodeToBase62(this long number)
     {
         if (number == 0) return Alphanumeric[0].ToString();
 
         var result = new Stack<char>();
         while (number > 0)
         {
-            result.Push(Alphanumeric[number % 62]);
+            result.Push(Alphanumeric[(int)(number % 62)]);
             number /= 62;
         }
         return new string(result.ToArray());
--- Api/tests/UrlShortener.Api.Core.Tests/Base62EncodingScenarios.cs
+++ Api/tests/UrlShortener.Api.Core.Tests/Base62EncodingScenarios.cs
@@ -11,7 +11,7 @@ public class Base62EncodingScenarios
     [InlineData(1000, "G8")]
     [InlineData(61, "z")]
     [InlineData(987654321, "14q60P")]
-    public void Should_encode_number_to_base62(int number, string expected)
+    public void Should_encode_number_to_base62(long number, string expected)
     {
         number.EncodeToBase62()
             .Should()
