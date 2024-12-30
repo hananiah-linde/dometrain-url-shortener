fee80c0a30e4ca9d86545ae49e6fa606876bca28
hananiah-linde <57416903+hananiah-linde@users.noreply.github.com>
2024-12-30 16:03:14 -0600

    update

--- Api/src/UrlShortener.Core/TokenProvider.cs
+++ Api/src/UrlShortener.Core/TokenProvider.cs
@@ -2,19 +2,23 @@ namespace UrlShortener.Core;
 
 public class TokenProvider
 {
-    private TokenRange? _tokenRange;
+    private static readonly Lock TokenLock = new();
+    private long _token = 0;
     public void AssignRange(int start, int end)
     {
-        _tokenRange = new TokenRange(start, end);
+        AssignRange(new TokenRange(start, end));
     }
 
     public void AssignRange(TokenRange tokenRange)
     {
-        _tokenRange = tokenRange;
+        _token = tokenRange.Start;
     }
 
     public long GetToken()
     {
-        return _tokenRange.Start;
+        lock (TokenLock)
+        {
+            return _token++;
+        }
     }
 }
