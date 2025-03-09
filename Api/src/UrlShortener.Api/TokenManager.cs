d56870bc74294253987c27b712d270583b02c376
hananiah-linde <hananiah.linde@gmail.com>
2025-03-09 11:16:07 -0500

    hosted service

--- Api/src/UrlShortener.Api/TokenManager.cs
+++ Api/src/UrlShortener.Api/TokenManager.cs
@@ -1,19 +1,48 @@
+using UrlShortener.Core;
+
 public class TokenManager : IHostedService
 {
     private readonly HttpClient _httpClient;
+    private readonly ILogger<TokenManager> _logger;
+    private TokenProvider _tokenProvider;
 
-    public TokenManager(IHttpClientFactory httpClientFactory)
+    private readonly string _machineIdentifier;
+
+    public TokenManager(IHttpClientFactory httpClientFactory, ILogger<TokenManager> logger, TokenProvider tokenProvider)
     {
+        _logger = logger;
+        _tokenProvider = tokenProvider;
         _httpClient = httpClientFactory.CreateClient("TokenRangeService");
+        _machineIdentifier = Environment.GetEnvironmentVariable("WEBSITE_INSTANCE_ID") ?? "unknown-machine-id";
     }
 
-    public Task StartAsync(CancellationToken cancellationToken)
+    public async Task StartAsync(CancellationToken cancellationToken)
     {
-        throw new NotImplementedException();
+        _logger.LogInformation("Starting token manager");
+
+        var response = await _httpClient.PostAsJsonAsync("assign",
+            new { Key = _machineIdentifier }, cancellationToken);
+
+        if (!response.IsSuccessStatusCode)
+        {
+            throw new Exception("Failed to assign new token range");
+        }
+
+        var range = await response.Content.ReadFromJsonAsync<TokenRange>(cancellationToken);
+
+        if (range is null)
+        {
+            throw new Exception("No tokens assigned");
+        }
+
+        _tokenProvider.AssignRange(range);
+
+        _logger.LogInformation("Token range assigned: {Start}-{End}", range.Start, range.End);
     }
 
     public Task StopAsync(CancellationToken cancellationToken)
     {
-        throw new NotImplementedException();
+        _logger.LogInformation("Stopping token manager");
+        return Task.CompletedTask;
     }
 }
