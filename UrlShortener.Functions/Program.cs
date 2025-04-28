f2723bbf8979a18aceb48164b1359d8fc642b541
hananiah-linde <57416903+hananiah-linde@users.noreply.github.com>
2025-04-28 15:46:25 -0500

    udpate
    update

--- UrlShortener.Functions/Program.cs
+++ UrlShortener.Functions/Program.cs
@@ -1,8 +1,18 @@
+using Azure.Identity;
+using Microsoft.Azure.Cosmos;
 using Microsoft.Azure.Functions.Worker.Builder;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
 var builder = FunctionsApplication.CreateBuilder(args);
-
+var keyVaultName = builder.Configuration["KeyVaultName"];
+if (!string.IsNullOrEmpty(keyVaultName))
+{
+    builder.Configuration.AddAzureKeyVault(
+        new Uri($"https://{keyVaultName}.vault.azure.net/"),
+        new DefaultAzureCredential());
+}
 builder.ConfigureFunctionsWebApplication();
 
 // Application Insights isn't enabled by default. See https://aka.ms/AAt8mw4.
@@ -10,4 +20,13 @@ builder.ConfigureFunctionsWebApplication();
 //     .AddApplicationInsightsTelemetryWorkerService()
 //     .ConfigureFunctionsApplicationInsights();
 
+builder.Services.AddSingleton<Container>(s =>
+{
+    var client = new CosmosClient(
+        connectionString: builder.Configuration["CosmosDb:ConnectionString"]
+    );
+    return client.GetContainer(
+        builder.Configuration["TargetDatabaseName"],
+        builder.Configuration["TargetContainerName"]);
+});
 builder.Build().Run();
--- infrastructure/modules/storage/cosmos-db.bicep
+++ infrastructure/modules/storage/cosmos-db.bicep
@@ -10,6 +10,10 @@ param containers array = [
     name: 'items'
     partitionKey: '/PartitionKey'
   }
+  {
+    name: 'byUser'
+    partitionKey: '/PartitionKey'
+  }
 ]
 
 resource cosmosDbAccount 'Microsoft.DocumentDB/databaseAccounts@2024-05-15' = {
@@ -82,4 +86,5 @@ resource cosmosDbConnectionString 'Microsoft.KeyVault/vaults/secrets@2023-07-01'
     value: cosmosDbAccount.listConnectionStrings().connectionStrings[0].connectionString
   }
 }
+
 output cosmosDbId string = cosmosDbAccount.id
